35d94317d75a1e0f0e770653d3e7412dc74d3ade
frank-qcd-qk <cxq41@case.edu>
2018-11-04 17:11:42 -0500

    Simplilfy process. function inside one file.

--- src/cxq41_ariac_boxMotion.cpp
+++ src/cxq41_ariac_boxMotion.cpp
@@ -11,8 +11,6 @@ using namespace std;
 bool g_take_new_snapshot = false;
 osrf_gear::LogicalCameraImage g_cam2_data; // Global information storage holder
 
-
-
 //! Call back for Camera2
 void camera2CallBack(const osrf_gear::LogicalCameraImage& message_Holder){
     if(g_take_new_snapshot){
@@ -22,23 +20,23 @@ void camera2CallBack(const osrf_gear::LogicalCameraImage& message_Holder){
     }
 }
 
-void start_competition(ros::NodeHandle & node){
-    ros::ServiceClient startup_client = n.serviceClient<std_srvs::Trigger>("/ariac/start_competition");
+void start_competition(ros::NodeHandle & startCompetitionNode){
+    ros::ServiceClient startup_client = startCompetitionNode.serviceClient<std_srvs::Trigger>("/ariac/start_competition");
     if(!startup_client.exists()){
         ROS_INFO("[Startup Phase]Waiting for the competition to be ready...");
         startup_client.waitForExistence();
         ROS_INFO("[Startup Phase]Competition is now ready...");
     }
     ROS_INFO("[Startup Phase]Requesting competition to start...");
-    startup_srv.response.success = false;
     std_srvs::Trigger startup_srv;
+    startup_srv.response.success = false;
     startup_client.call(startup_srv);
     while(!startup_srv.response.success){
-        ROS_WARN("Not successful startup yet... Repeat Calling...");
+        ROS_WARN("[Startup Phase]Not successful startup yet... Repeat Calling...");
         startup_client.call(startup_srv);
         ros::Duration(0.5).sleep();
     }
-    ROS_INFO("Competition Startup Success");
+    ROS_INFO("[Startup Phase]Competition Startup Success>>>[Belt Control Phase]");
 }
 
 
@@ -53,9 +51,8 @@ int main(int argc, char **argv) {
     ros::Subscriber camera2= n.subscribe("/ariac/logical_camera_2",1,camera2CallBack); 
 
     
-    //! System Startup Call
-
-    // ! End of System Startup Call
+    //! Competition Startup Call
+    start_competition(n);
 
     while(ros::ok()){
         //* Start the conveyor before anything:
@@ -68,12 +65,12 @@ int main(int argc, char **argv) {
         }
         ROS_INFO("Conveyor Startup Success");
 
-        while(g)g_cam2_data.models.size()>0){
+        while(g_cam2_data.models.size()>0){
             ROS_INFO("The current box z position is: %f",g_cam2_data.pose.position.z);
             ros::spinOnce(); //allow data from call back
             ros::Duration(0.5).sleep();
         }
-
+        ros::spinOnce();
     }
 
 
